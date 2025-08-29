0b6a6542faae975d4d1bacef5bc30806fa708979
Konstantin <beeters88@gmail.com>
2025-08-29 16:17:51 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,29 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  const space = '**********';
+  const arr = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    let part = expr.slice(i, i + 10);
+    if (part.indexOf('1') !== -1) {
+      const index = part.indexOf('1');
+      part = part.slice(index);
+      const subArr = [];
+      for (let j = 0; j < part.length; j += 2) {
+        const dot = '.';
+        const dash = '-';
+        const sliced = part.slice(j, j + 2);
+        if (sliced === '10') {
+          subArr.push(dot);
+        } else {
+          subArr.push(dash);
+        }
+      }
+      arr.push(subArr.join(''));
+    } else if (part === space) arr.push(' ');
+  }
+
+  return arr.map((elem) => MORSE_TABLE[elem] || elem).join('');
 };
